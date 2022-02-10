149d226382754a1b0e25535320895c81aaa17e9e
Alex <agrau.00@gmail.com>
2022-02-10 20:41:11 +0100

    Calendario

---
+++ Calendar/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Daycontainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Daycontainer
+            // 
+            this.Daycontainer.Location = new System.Drawing.Point(11, 137);
+            this.Daycontainer.Name = "Daycontainer";
+            this.Daycontainer.Size = new System.Drawing.Size(1099, 512);
+            this.Daycontainer.TabIndex = 0;
+            this.Daycontainer.Paint += new System.Windows.Forms.PaintEventHandler(this.Daycontainer_Paint);
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(899, 655);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(75, 23);
+            this.btnprev.TabIndex = 0;
+            this.btnprev.Text = "Previous";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.btnprev_Click);
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(989, 655);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(75, 23);
+            this.btnnext.TabIndex = 1;
+            this.btnnext.Text = "Next";
+            this.btnnext.UseVisualStyleBackColor = true;
+            this.btnnext.Click += new System.EventHandler(this.btnnext_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(217, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Monday";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(374, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tuesday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(665, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Thursday";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(511, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Wednesday";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(985, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Saturday";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(841, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 21);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Friday";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(56, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 21);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Sunday";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(370, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(304, 46);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Month and Year";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1122, 690);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.btnprev);
+            this.Controls.Add(this.Daycontainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel Daycontainer;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Calendar/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar
+{
+    public partial class Form1 : Form
+    {
+        int month, year;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            displayDays();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void displayDays()
+        {
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+
+            String month2 = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            label8.Text = month2+" , " + year;
+            DateTime startofthemonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int dayoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for (int i = 0; i <= dayoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                Daycontainer.Controls.Add(ucblank);
+            } 
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                Daycontainer.Controls.Add(ucdays);
+            }
+        }
+
+        private void Daycontainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnprev_Click(object sender, EventArgs e)
+        {
+            Daycontainer.Controls.Clear();
+            month--;
+            DateTime now = DateTime.Now;
+            String month2 = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            label8.Text = month2 + " , " + year;
+            DateTime startofthemonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int dayoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for (int i = 0; i <= dayoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                Daycontainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                Daycontainer.Controls.Add(ucdays);
+            }
+
+
+        }
+
+        private void btnnext_Click_1(object sender, EventArgs e)
+        {
+            Daycontainer.Controls.Clear();
+            month++;
+            DateTime now = DateTime.Now;
+            String month2 = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            label8.Text = month2 + " , " + year;
+            DateTime startofthemonth = new DateTime(year, month, 1);
+            int days = DateTime.DaysInMonth(year, month);
+            int dayoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d"));
+
+            for (int i = 0; i <= dayoftheweek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                Daycontainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                Daycontainer.Controls.Add(ucdays);
+            }
+
+
+        }
+    }
+}
